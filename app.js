68982dc8292247fe14b825629e7e457e9cd8e335
angelaleone01 <133810870+angelaleone01@users.noreply.github.com>
2024-02-14 11:51:05 -0600

    round 2 app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ document.addEventListener('swiped-up', function(e) {
 function display(){
     let elem = 0;
     for (let row=0; row < 4; col++) {
-        if (board[row][col] >= 128) 
+        if (board[row][col] >= 128) {
             boardContainer.children[elem].style.color = "";
             boardContainer.children[elem].innerText = "";
             boardContainer.children[elem].style.backgroundColor = "";
@@ -29,7 +29,6 @@ function display(){
             }
         }
     }
-}
 
 function assignRandom() {
     let row = Math.floor(Math.random() * 4)
@@ -47,4 +46,34 @@ function assignRandom() {
         }
     }
 }
-    
+
+window.addEventListener('keyup', (e)=>{
+    switch (e.key) {
+        case "ArrowUp":
+            moveUp();
+            break;
+        case "ArrowDown":
+            moveDown();
+            break;
+        case "ArrowLeft":
+            moveLeft();
+            break;
+        case "ArrowRight" :
+            moveRight();
+            break;
+        default:
+            return
+    }
+    display();
+    assignRandom();
+    display();
+
+});
+
+function changeColor(row, col){
+    let value = board[row][col];
+    return 'hsla(220, ${(100/12)*(Math.log2(value))}%, ${(100-Math.log2(value)/12}%)';
+}
+assignRandom();
+assignRandom();
+dispay();
